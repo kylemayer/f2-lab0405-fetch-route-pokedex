4f68489ff7860565d5555eb6abacd27196de699e
Kyle Mayer <x@kylemayer.dev>
2021-06-14 09:22:45 -0700

    changed shape data prop

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -10,7 +10,7 @@ export default class PokeItem extends Component {
                 <p>Type: {this.props.type}</p>
                 <p>Height: {this.props.height}</p>
                 <p>Weight: {this.props.weight}</p>
-                <p>Shape: {this.props.weight}</p>
+                <p>Shape: {this.props.shape}</p>
                 <p>Ability: {this.props.ability_hidden}</p>
                 </div>
 
