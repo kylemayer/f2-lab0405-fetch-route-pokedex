cb906a0e2e663f635a278020bfd92e50803ce048
Kyle Mayer <x@kylemayer.dev>
2021-06-18 10:35:10 -0700

    fixed asc/desc, pokedetails loading spinner, pagination, and the search function  after I broke it during refactor

--- src/PokeIndex.js
+++ src/PokeIndex.js
@@ -9,11 +9,11 @@ const sleep = (x) => new Promise((res, rej) => setTimeout(() => { res() }, x))
 
 export default class PokeIndex extends Component {
 state = {
-    pokemonData: [],
+    pokemon: [],
     loading: false,
     query: '',
     type: '',
-    direction: '',
+    direction: 'asc',
     page: 1,
 }
 
@@ -29,8 +29,8 @@ handleTypeChange =  (e) => {
     this.setState({type: e.target.value})
 }
 
-handleDirectionChange = (e) => {
-    this.setState({direction: e.target.value})
+handleDirectionChange = async (e) => {
+    await this.setState({direction: e.target.value})
 }
 
 nextPage = async (e) => {
@@ -43,42 +43,40 @@ prevPage = async (e) => {
     this.fetch()
 }
 
-handleClick = async () => {
-    await this.fetch()
-}
 
 fetch = async () => {
     this.setState({loading : true})
 
-    const URL = new URLSearchParams({
-        sort: 'pokemonData',
+    const searchParams = new URLSearchParams({
+        sort: 'pokemon',
         direction: this.state.direction,
         page: this.state.page,
         type_1: this.state.type,
     })
 
     if (this.state.query) {
-        URL.set('pokemonData', this.state.query);
+        searchParams.set('pokemon', this.state.query);
     }
 
     const {
-        body: { results: data }
-    }   = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?${URL.toString()}`)
+        body: {results: data},
+        } = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?${searchParams.toString()}`
+    )
 
-        await sleep(1500);
-        this.setState({loading : false});
-        this.setState({pokemonData: data})
+    await sleep(1000)
+    this.setState({loading : false})
+    this.setState({pokemon: data})
 }
 
 render() {
     return (
         <div className="App">
             <label className="user-input">Search for Pokemon
-                <input placeholder="search name" onChange={this.handleSearch}/>
+                <input placeholder="search name" id="search" onChange={this.handleSearch}/>
             </label>
             <DropdownType handleType={this.handleTypeChange}/>
             <DropdownDir handleDirection={this.handleDirectionChange}/>
-            <button onClick={this.handleClick}>SEARCH 🔍</button>
+            <button onClick={this.fetch}>SEARCH 🔍</button>
             {this.state.page - 1 > 0 && (
                     <button onClick={this.prevPage}>
                         Prev Page ({this.state.page - 1})
@@ -89,7 +87,7 @@ render() {
                 </button>
             {this.state.loading
                 ? <Spinner />
-                : <PokeList pokeData={this.state.pokemonData}/>}
+                : <PokeList pokeData={this.state.pokemon}/>}
             {this.state.page - 1 > 0 && (
                 <button onClick={this.prevPage}>
                     Prev Page ({this.state.page - 1})
